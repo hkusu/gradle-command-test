81bf0ae0fe2d70eaf0e56950d4e8e752057a24fa
Hiroyuki Kusu <kusu0806@gmail.com>
2023-10-31 13:02:59 +0900

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,21 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "My Application"
+include(":app")
+include(":mylibrary1")
+include(":mylibrary2")
+include(":lib1")
+include(":lib2")
