8fa145df4420b3a83b830f63c208ccaf814ba343
Hiroyuki Kusu <kusu0806@gmail.com>
2023-10-31 14:30:42 +0900

    javaモジュールにテストコード追加

--- lib1/build.gradle.kts
+++ lib1/build.gradle.kts
@@ -7,3 +7,6 @@ java {
     sourceCompatibility = JavaVersion.VERSION_17
     targetCompatibility = JavaVersion.VERSION_17
 }
+dependencies {
+    implementation("junit:junit:4.12")
+}
---
+++ lib2/src/test/java/com/example/lib2/MyClassTest.kt
@@ -0,0 +1,9 @@
+import org.junit.Assert
+import org.junit.Test
+
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        Assert.assertEquals(4, 2 + 2)
+    }
+}
